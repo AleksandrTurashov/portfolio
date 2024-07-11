b3016dbeda66901a33cdf014ea939f9c8fc6765b
aleksandr <shurik.turashov@mail.ru>
2024-07-11 19:33:56 +0300

    strange error

--- stamina_php/Views/pages/index.php
+++ stamina_php/Views/pages/index.php
@@ -9,32 +9,38 @@
 <body>
     <div class="wrap">
        
-            <?php MenuWidget::render('sideBarNav'); ?>
+            <?php 
+            // MenuWidget::render('sideBarNav'); 
+            ?>
 
         <header>
 
-            <?php MenuWidget::render('mainNav'); ?>
+            <?php 
+            // MenuWidget::render('mainNav'); 
+            ?>
 
         </header>
         <div class="main_wrap" id="main_content">
 
             <?php 
-                HomeSection::render();
-                HealthiesSection::render();
-                MotivationWidget::render(1);               
-                ClassesSection::render();         
-                MotivationWidget::render(2);
-                SheduleSection::render();
-                MotivationWidget::render(3);       
-                TrainersSection::render();
-                ServicesSection::render();
-                ContactSection::render();
+                // HomeSection::render();
+                // HealthiesSection::render();
+                // MotivationWidget::render(1);               
+                // ClassesSection::render();         
+                // MotivationWidget::render(2);
+                // SheduleSection::render();
+                // MotivationWidget::render(3);       
+                // TrainersSection::render();
+                // ServicesSection::render();
+                // ContactSection::render();
             ?>
 
         </div>
         <footer id="footer">
 
-            <?php Footer::render(); ?>
+            <?php 
+            // Footer::render();
+             ?>
 
         </footer>
     </div>
